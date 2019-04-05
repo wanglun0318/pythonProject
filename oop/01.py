cfac82e31b52be1badeb479f00720ad52d90f342
wanglun0318 <lang_1273@qq.com>
2019-04-05 21:26:19 +0800

    test

--- oop/01.py
+++ oop/01.py
@@ -1,17 +1,25 @@
- # 定义一个类，用来描述学生
-class  PythonStudent():
-     name = "月夜"
-     age = 19
-     course = "python"
 
-     # 定一个学生行为的方法，注意缩进层级
-     def doHomework(self):
-         print("{0}在做作业，是个好好孩子呢......")
+"""
+定义一个学生类
+"""
 
-         #推荐在函数末尾使用return语句
-         return None
+
+# 定义一个类，用来描述学生
+class PythonStudent():
+    name = "月夜"
+    age = 19
+    course = "python"
+
+
+# 定一个学生行为的方法，注意缩进层级
+    def doHomework(self):
+
+        print("在做作业，是个好好孩子呢......")
+# 推荐在函数末尾使用return语句
+        return None
 
 # 实例化一个学生
+
 
 student1 = PythonStudent()
 print(student1.name)
