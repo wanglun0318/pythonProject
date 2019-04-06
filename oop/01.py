fa8ea57f1f69df2c45d89355a385e03106904e84
wanglun0318 <lang_1273@qq.com>
2019-04-06 22:07:48 +0800

    test

--- oop/01.py
+++ oop/01.py
@@ -18,6 +18,19 @@ class PythonStudent():
 # 推荐在函数末尾使用return语句
         return None
 
+# 构造函数
+    def __init__(self):
+        self.name = "aaaa"
+        self.age = 200
+
+    def say(self):
+        print(self.name)
+        print(self.age)
+
+class B():
+    name = "bbbbbb"
+    age = 90
+
 # 实例化一个学生
 
 
@@ -26,3 +39,17 @@ print(student1.name)
 print(student1.age)
 
 student1.doHomework()
+
+# 此时 系统会默认把a作为第一个参数传入函数
+a = PythonStudent()
+a.say()
+
+# 此时self被a替换
+PythonStudent.say(a)
+
+# 同样可以把PythonStudent作为参数传入
+PythonStudent.say(PythonStudent)
+
+# 此时传入的是类实例B,因为B具有name和age不会报错,此时利用了鸭子模型
+PythonStudent.say(B)
+
