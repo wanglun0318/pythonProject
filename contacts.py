8cf43d97bef6df034a03a3b582a0adc0511640df
wanglun0318 <lang_1273@qq.com>
2019-04-21 22:09:19 +0800

    inherit

---
+++ contacts.py
@@ -0,0 +1,46 @@
+print("|--- 欢迎进入通讯路程序 ---|")
+print("|--- 1：查询联系人资料  ---|")
+print("|--- 2：插入新的联系人  ---|")
+print("|--- 3：删除已有联系人  ---|")
+print("|--- 4：退出通讯录程序  ---|")
+
+contacts = dict()
+instruct = int(input("请输入相关指令代码："))
+
+def con(instruct):
+    if instruct == 1:
+        print(contacts)
+        print(type(contacts))
+        a = int(input("请输入相关指令代码："))
+        return a
+    elif instruct == 2:
+        conkey = input("请输入联系人姓名：")
+        if conkey in contacts:
+            print("您输入的联系人已经存在，")
+            if input("是否修改电话号码（YES/NO):") == "YES":
+                contacts[conkey] = input("请输入用户联系电话")
+        else:
+            contacts[conkey] = input("请输入用户联系电话")
+        print(contacts)
+        a = int(input("请输入相关指令代码："))
+        return a
+    elif instruct == 3:
+        conkey = input("请输入要删除的联系人姓名：")
+        del contacts[conkey]
+        print(contacts)
+        a = int(input("请输入相关指令代码："))
+        return a
+    elif instruct == 4:
+        print("|--- 感谢使用通讯录  ---|")
+        exit()
+    else:
+        print("输入有误，程序关闭！！！！")
+        exit()
+
+
+instruct = con(instruct)
+
+while instruct != 4:
+    instruct = con(instruct)
+else:
+    print("|--- 感谢使用通讯录  ---|")
--- helloword.py
+++ helloword.py
@@ -1,1 +1,32 @@
-print("大家好！！！")
+# 引入随机模块random
+import random
+
+
+print("大家好,我们来玩一个游戏......")
+print("=======心有灵犀==========")
+print("这是一个猜数字的游戏，看看你能不能猜出我心里的数字！！！")
+# 生成一个随机数
+secret = random.randint(0,10)
+temp = input("请输入你猜的数字:")
+# isis = isinstance(temp,int)
+
+guess = int(temp)
+
+while  guess != secret:
+    temp = input("没猜对哦，继续再猜猜:")
+    guess = int(temp)
+    if guess == secret:
+        print("恭喜你猜对了，看来咱俩真是心有灵犀！！！")
+        print("=================游戏结束===================")
+        exit()
+    else:
+        if guess > secret:
+            print("哟，大了一点哦，马上就要猜对了......")
+            print("====================================")
+        else:
+            print("哟，小了一点哦，马上要猜对了......")
+            print("====================================")
+
+
+print("哟，一下就猜中了，真是心有灵犀一点通啊！")
+print("=================游戏结束===================")
